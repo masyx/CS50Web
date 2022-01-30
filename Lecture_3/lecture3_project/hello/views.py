e5e2d7882b7b3addf9d74f62fefeedc781905460
Sergey Matveev <rfmatveev@gmail.com>
2022-01-30 11:41:09 -0600

    Hello app finished

--- Lecture_3/lecture3_project/hello/views.py
+++ Lecture_3/lecture3_project/hello/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, Django here")
+
+def sasha(request):
+    return HttpResponse("Hello, Sasha")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize(),
+    })
