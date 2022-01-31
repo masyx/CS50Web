5d941dc8c5dc4c17afa85bfc36f313354cfc3649
Sergey Matveev <rfmatveev@gmail.com>
2022-01-31 08:00:46 -0600

    newyear app done

--- Lecture_3/lecture3_project/lecture3_project/urls.py
+++ Lecture_3/lecture3_project/lecture3_project/urls.py
@@ -12,11 +12,12 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+""" 
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
+    path('hello/', include("hello.urls")),
+    path('isitnewyear/', include("newyear.urls"))
 ]
