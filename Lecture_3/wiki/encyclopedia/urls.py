e01ec00550bfcfada12ce3adf6c2a90067b8b579
Sergey Matveev <rfmatveev@gmail.com>
2022-02-14 09:16:41 -0600

    wiki progress

--- Lecture_3/wiki/encyclopedia/urls.py
+++ Lecture_3/wiki/encyclopedia/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:page_name>", views.get_page, name="wiki_entry")
 ]
--- Lecture_3/wiki/encyclopedia/views.py
+++ Lecture_3/wiki/encyclopedia/views.py
@@ -7,4 +7,10 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def get_page(request, page_name):
+    return render(request, "encyclopedia/entry.html", {
+        "page_name": page_name.upper(),
+        "entry": util.get_entry(page_name)
+    })
 
